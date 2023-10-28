cc343061234eaf545097e0e75c63ca3ac3715365
karina-tkach <ms.karinatk@gmail.com>
2023-10-28 03:29:51 +0300

    L02 Added methods to collect students' information and check input data for correctness

--- Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
+++ Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
@@ -1,6 +1,52 @@
 package org.geekhub.hw2;
 
-public class ApplicationStarter {
+import java.util.Scanner;
 
-    // TODO write code here AND REMOVE THIS MESSAGE
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("The number of students is not provided");
+            System.exit(0);
+        } else {
+            int numberOfStudents = Integer.parseInt(args[0]);
+            if (numberOfStudents <= 0) {
+                System.out.println("The number of students must greater than zero");
+                System.exit(0);
+            } else {
+               //
+            }
+        }
+    }
+
+    public static String[] collectStudentsInfo(int numberOfStudents) {
+        Scanner scanner = new Scanner(System.in);
+        final int NUMBER_OF_PROPERTIES = 4;
+        String[] info = new String[numberOfStudents * NUMBER_OF_PROPERTIES];
+        for (int i = 1, j = 0; i <= numberOfStudents; i++, j += 4) {
+            System.out.printf("Enter information for student #%d%n", i);
+            info[j] = getStudentInfoAndCheck("Student's Name", scanner);
+            info[j + 1] = getStudentInfoAndCheck("Math Grade (0-100)", scanner);
+            info[j + 2] = getStudentInfoAndCheck("Science Grade (0-100)", scanner);
+            info[j + 3] = getStudentInfoAndCheck("History Grade (0-100)", scanner);
+        }
+        scanner.close();
+        return info;
+    }
+
+    public static String getStudentInfoAndCheck(String infoName, Scanner scanner) {
+        boolean isWrong;
+        String property;
+        do {
+            System.out.printf("%s: ", infoName);
+            property = scanner.nextLine();
+            isWrong = property.isBlank();
+            if (!(isWrong || infoName.equals("Student's Name"))) {
+                isWrong = !(Integer.parseInt(property) >= 0 && Integer.parseInt(property) <= 100);
+            }
+            if (isWrong) {
+                System.out.printf("%s is wrong, please input it again%n", infoName);
+            }
+        } while (isWrong);
+        return property;
+    }
 }
