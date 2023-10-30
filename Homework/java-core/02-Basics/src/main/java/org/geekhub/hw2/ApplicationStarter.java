37ba4066c8c54f598308ffa215958f99d7dc15b5
karina-tkach <ms.karinatk@gmail.com>
2023-10-31 00:45:35 +0200

    L02 Changed the code according to the reviewer's comments

--- Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
+++ Homework/java-core/02-Basics/src/main/java/org/geekhub/hw2/ApplicationStarter.java
@@ -6,66 +6,77 @@ public class ApplicationStarter {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("The number of students is not provided");
-            System.exit(0);
+            System.exit(-1);
         } else {
             int numberOfStudents = Integer.parseInt(args[0]);
             if (numberOfStudents <= 0) {
-                System.out.println("The number of students must greater than zero");
-                System.exit(0);
+                System.out.println("The number of students must be greater than zero");
+                System.exit(-2);
             } else {
-                String[] studentsInfo = collectStudentsInfo(numberOfStudents);
-                displayAverageGrades(studentsInfo, numberOfStudents);
+                Scanner scanner = new Scanner(System.in);
+                String[] gradesNames = {"Math Grade (0-100)", "Science Grade (0-100)", "History Grade (0-100)"};
+                String[] studentsNames = new String[numberOfStudents];
+                int[][] studentsGrades = new int[numberOfStudents][];
+                for (int i = 0; i < numberOfStudents; i++) {
+                    System.out.printf("Enter information for student #%d%n", i + 1);
+                    studentsNames[i] = getStudentNameAndCheck(scanner);
+                    studentsGrades[i] = getStudentGradesAndCheck(gradesNames, scanner);
+                }
+                scanner.close();
+                displayAverageGrades(studentsNames, studentsGrades, numberOfStudents);
             }
         }
     }
 
-    public static String[] collectStudentsInfo(int numberOfStudents) {
-        Scanner scanner = new Scanner(System.in);
-        final int NUMBER_OF_PROPERTIES = 4;
-        String[] info = new String[numberOfStudents * NUMBER_OF_PROPERTIES];
-        for (int i = 1, j = 0; i <= numberOfStudents; i++, j += 4) {
-            System.out.printf("Enter information for student #%d%n", i);
-            info[j] = getStudentInfoAndCheck("Student's Name", scanner);
-            info[j + 1] = getStudentInfoAndCheck("Math Grade (0-100)", scanner);
-            info[j + 2] = getStudentInfoAndCheck("Science Grade (0-100)", scanner);
-            info[j + 3] = getStudentInfoAndCheck("History Grade (0-100)", scanner);
-        }
-        scanner.close();
-        return info;
-    }
-
-    public static String getStudentInfoAndCheck(String infoName, Scanner scanner) {
+    public static String getStudentNameAndCheck(Scanner scanner) {
         boolean isWrong;
-        String property;
+        String name;
         do {
-            System.out.printf("%s: ", infoName);
-            property = scanner.nextLine();
-            isWrong = property.isBlank();
-            if (!(isWrong || infoName.equals("Student's Name"))) {
-                isWrong = !(Integer.parseInt(property) >= 0 && Integer.parseInt(property) <= 100);
-            }
+            System.out.print("Student's Name: ");
+            name = scanner.nextLine();
+            isWrong = name.isBlank();
             if (isWrong) {
-                System.out.printf("%s is wrong, please input it again%n", infoName);
+                System.out.println("Student's Name is wrong, please input it again");
             }
         } while (isWrong);
-        return property;
+        return name;
     }
 
-    public static void displayAverageGrades(String[] studentsInfo, int numberOfStudents) {
+    public static int[] getStudentGradesAndCheck(String[] gradesNames, Scanner scanner) {
+        final int NUMBER_OF_GRADES = 3;
+        int[] studentGrades = new int[NUMBER_OF_GRADES];
+        boolean isWrong;
+        int grade;
+        for (int i = 0; i < NUMBER_OF_GRADES; i++) {
+            do {
+                System.out.printf("%s: ", gradesNames[i]);
+                //we use Integer.parseInt(), so not to skip next nextLine() call
+                grade = Integer.parseInt(scanner.nextLine());
+                isWrong = !(grade >= 0 && grade <= 100);
+                if (isWrong) {
+                    System.out.printf("%s is wrong, please input it again%n", gradesNames[i]);
+                }
+            } while (isWrong);
+            studentGrades[i] = grade;
+        }
+        return studentGrades;
+    }
+
+    public static void displayAverageGrades(String[] studentsNames, int[][] studentsGrades, int numberOfStudents) {
         double sumOfMathGrades = 0;
         double sumOfScienceGrades = 0;
         double sumOfHistoryGrades = 0;
         System.out.println();
         System.out.println("Average Grades for Each Student:");
-        for (int i = 0; i < studentsInfo.length; i += 4) {
-            int mathGrade = Integer.parseInt(studentsInfo[i + 1]);
+        for (int i = 0; i < numberOfStudents; i++) {
+            int mathGrade = studentsGrades[i][0];
             sumOfMathGrades += mathGrade;
-            int scienceGrade = Integer.parseInt(studentsInfo[i + 2]);
+            int scienceGrade = studentsGrades[i][1];
             sumOfScienceGrades += scienceGrade;
-            int historyGrade = Integer.parseInt(studentsInfo[i + 3]);
+            int historyGrade = studentsGrades[i][2];
             sumOfHistoryGrades += historyGrade;
             double average = calculateAverage(mathGrade, scienceGrade, historyGrade);
-            System.out.printf("%s: %f%n", studentsInfo[i], average);
+            System.out.printf("%s: %f%n", studentsNames[i], average);
         }
         System.out.println();
         System.out.println("Average Grades for Each Subject:");
