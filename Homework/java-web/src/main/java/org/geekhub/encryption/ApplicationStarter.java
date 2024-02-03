f56b8fda09e4a0788209aafaed97590e4fbc5322
karina-tkach <ms.karinatk@gmail.com>
2024-02-03 23:33:04 +0200

    L14 Changed main class so to test application.

--- Homework/java-web/src/main/java/org/geekhub/encryption/ApplicationStarter.java
+++ Homework/java-web/src/main/java/org/geekhub/encryption/ApplicationStarter.java
@@ -1,14 +1,12 @@
 package org.geekhub.encryption;
 
 import org.geekhub.encryption.models.HistoryEntry;
-import org.geekhub.encryption.repository.EncryptionRepositoryInMemory;
 import org.geekhub.encryption.service.EncryptionService;
 import org.geekhub.encryption.service.LogService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+import java.util.List;
 
 public class ApplicationStarter {
 
@@ -16,18 +14,45 @@ public class ApplicationStarter {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("org.geekhub.encryption.configurations");
         context.registerShutdownHook();
 
-
         EncryptionService encryptionService = context.getBean(EncryptionService.class);
         String originalMessage = "Hello";
         String encryptedMessage = encryptionService.performOperation(originalMessage);
         System.out.printf("%s%n", encryptedMessage);
-        /*OffsetDateTime from = OffsetDateTime.of(2024,2,3,20,0,0,0, ZoneOffset.ofHours(2));
-        OffsetDateTime to = OffsetDateTime.of(2024,2,3,20,01,48,161196, ZoneOffset.ofHours(2));
+
         LogService logService = context.getBean(LogService.class);
-        for(HistoryEntry entry : logService.getHistoryInDateRange(from,null)){
+
+        printFetchName("History by algorithm:");
+        printHistory(logService.getHistoryByAlgorithm("Caesar"));
+        printDelimiters();
+
+        printFetchName("History in date range:");
+        printHistory(logService.getHistoryInDateRange(null, OffsetDateTime.now()));
+        printDelimiters();
+
+        printFetchName("History bt algorithm and operation type:");
+        printHistory(logService.getHistoryByAlgorithmAndOperationType("Vigenere", "ENCRYPTION"));
+        printDelimiters();
+
+        printFetchName("History with pagination:");
+        printHistory(logService.getFullHistoryWithPagination(1, 2));
+        printDelimiters();
+
+        printFetchName("History with pagination and user id:");
+        printHistory(logService.getFullHistoryWithPaginationAndUserId(1, 1, 2));
+        printDelimiters();
+    }
+
+    private static void printHistory(List<HistoryEntry> historyEntryList) {
+        for (HistoryEntry entry : historyEntryList) {
             System.out.println(entry);
-        }*/
+        }
+    }
 
+    private static void printDelimiters() {
+        System.out.println("\n\n\n");
+    }
 
+    private static void printFetchName(String fetchName) {
+        System.out.println(fetchName);
     }
 }
