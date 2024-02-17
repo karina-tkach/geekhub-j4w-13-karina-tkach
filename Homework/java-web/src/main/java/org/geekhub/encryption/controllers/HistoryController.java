3a483c19594461a3df4d46cd4d7d85c232f82d5f
karina-tkach <ms.karinatk@gmail.com>
2024-02-17 23:46:45 +0200

    L16 Added pagination to history.

--- Homework/java-web/src/main/java/org/geekhub/encryption/controllers/HistoryController.java
+++ Homework/java-web/src/main/java/org/geekhub/encryption/controllers/HistoryController.java
@@ -4,6 +4,7 @@ import org.geekhub.encryption.service.HistoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HistoryController {
@@ -16,6 +17,14 @@ public class HistoryController {
     @GetMapping(path="/history")
     public String history(Model model) {
         model.addAttribute("entries", historyService.getFullHistory());
+        return "history";
+    }
+
+    @GetMapping(path="/history/{limit}/{pageNumber}")
+    public String historyWithPagination(@PathVariable int limit,
+                                        @PathVariable int pageNumber,
+                                        Model model) {
+        model.addAttribute("entries", historyService.getFullHistoryWithPagination(pageNumber, limit));
         return "history";
     }
 }
