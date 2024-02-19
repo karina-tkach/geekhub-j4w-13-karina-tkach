30b3579cb9869e95ad64e80ac51b1eb68400cbee
karina-tkach <ms.karinatk@gmail.com>
2024-02-19 17:26:02 +0200

    L16 Added back buttons and their style.

--- Homework/java-web/src/main/java/org/geekhub/encryption/controllers/HistoryController.java
+++ Homework/java-web/src/main/java/org/geekhub/encryption/controllers/HistoryController.java
@@ -34,7 +34,7 @@ public class HistoryController {
                                         Model model) {
         List<HistoryEntry> history = historyService.getFullHistoryWithPagination(pageNumber, limit);
         model.addAttribute("pageNumber", pageNumber);
-        model.addAttribute("totalPages",history.size()/limit);
+        model.addAttribute("totalPages",Math.round(historyService.getFullHistory().size()/(float)limit));
         model.addAttribute("entries", history);
         return "historyPagination";
     }
